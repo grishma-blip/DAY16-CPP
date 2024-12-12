6a890d118f6a9bfc6460d7f24cfcddeadad49371
Grishma Thakare <85503530+grishma-blip@users.noreply.github.com>
2024-12-12 09:34:36 +0530

    Add files via upload

---
+++ inhertance1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class vehicle {
+    public:
+     vehicle() {
+        cout << "this is vehicle" << endl;
+     }
+};
+class car : public vehicle {
+};
+int main() {
+    car obj;
+    return 0;
+}
